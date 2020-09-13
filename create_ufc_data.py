9b1ab6e8e9e06ff4c6ee91a498d10bed3d62758c
WarrierRajeev <warrier.rajeev.95@gmail.com>
2020-09-13 22:30:08 +0530

    Rename and Refactor file to create ufc data

---
+++ create_ufc_data.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+from createdata.scrape_fight_data import create_fight_data_csv
+from createdata.scrape_fight_links import UFCLinks
+from createdata.scrape_fighter_details import create_fighter_data_csv
+
+from createdata.data_files_path import (  # isort:skip
+    NEW_EVENT_AND_FIGHTS,
+    TOTAL_EVENT_AND_FIGHTS,
+)
+
+ufc_links = UFCLinks()
+new_events_and_fight_links, all_events_and_fight_links = (
+    ufc_links.get_event_and_fight_links()
+)
+print("Scraped and saved event and fight links!")
+
+if not new_events_and_fight_links:
+    create_fight_data_csv(new_events_and_fight_links, filename=TOTAL_EVENT_AND_FIGHTS)
+else:
+    create_fight_data_csv(new_events_and_fight_links, filename=NEW_EVENT_AND_FIGHTS)
+
+    create_fighter_data_csv()
+
+    new_event_and_fights_data = pd.read_csv(NEW_EVENT_AND_FIGHTS)
+    old_event_and_fights_data = pd.read_csv(TOTAL_EVENT_AND_FIGHTS)
+
+    latest_total_fight_data = new_event_and_fights_data.append(
+        old_event_and_fights_data, ignore_index=True
+    )
+    latest_total_fight_data.to_csv(TOTAL_EVENT_AND_FIGHTS, index=None)
+
+create_fighter_data_csv()
--- scrape_all_data.py
+++
@@ -1,8 +0,0 @@
-from createdata.scrape_fight_links import get_all_links
-from createdata.scrape_fight_data import create_fight_data_csv
-from createdata.scrape_fighter_details import create_fighter_data_csv
-
-
-event_and_fight_links = get_all_links()
-create_fight_data_csv(event_and_fight_links)
-create_fighter_data_csv()
