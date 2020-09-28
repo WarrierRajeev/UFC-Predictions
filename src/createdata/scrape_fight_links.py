46bc5a82a0d150dccdc4c9a8fe960ed586d6451e
WarrierRajeev <warrier.rajeev.95@gmail.com>
2020-09-28 10:05:50 +0530

    Refactor UFCLinks class

--- src/createdata/scrape_fight_links.py
+++ src/createdata/scrape_fight_links.py
@@ -37,31 +37,31 @@ class UFCLinks:
             # Find links of the newer events
             new_event_links = list(set(all_event_links) - set(past_event_links))
 
-            # dump all_event_links as PAST_EVENT_LINKS
-            pickle_out1 = open(PAST_EVENT_LINKS_PICKLE_PATH.as_posix(), "wb")
-            pickle.dump(all_event_links, pickle_out1)
-            pickle_out1.close()
+        # dump all_event_links as PAST_EVENT_LINKS
+        pickle_out1 = open(PAST_EVENT_LINKS_PICKLE_PATH.as_posix(), "wb")
+        pickle.dump(all_event_links, pickle_out1)
+        pickle_out1.close()
 
         return new_event_links, all_event_links
 
-    def get_fight_links(self, event_links: List[str]) -> Dict[str, List[str]]:
-        event_and_fight_links = {}
-        for link in event_links:
-            event_fights = []
-            soup = make_soup(link)
-            for row in soup.findAll(
-                "tr",
-                {
-                    "class": "b-fight-details__table-row b-fight-details__table-row__hover js-fight-details-click"
-                },
-            ):
-                href = row.get("data-link")
-                event_fights.append(href)
-            event_and_fight_links[link] = event_fights
-
-        return event_and_fight_links
-
     def get_event_and_fight_links(self) -> (Dict, Dict):
+        def get_fight_links(event_links: List[str]) -> Dict[str, List[str]]:
+            event_and_fight_links = {}
+            for link in event_links:
+                event_fights = []
+                soup = make_soup(link)
+                for row in soup.findAll(
+                    "tr",
+                    {
+                        "class": "b-fight-details__table-row b-fight-details__table-row__hover js-fight-details-click"
+                    },
+                ):
+                    href = row.get("data-link")
+                    event_fights.append(href)
+                event_and_fight_links[link] = event_fights
+
+            return event_and_fight_links
+
         new_events_and_fight_links = {}
         if EVENT_AND_FIGHT_LINKS_PICKLE_PATH.exists():
             if not self.new_event_links:
@@ -70,9 +70,9 @@ class UFCLinks:
                 pickle_in.close()
                 return new_events_and_fight_links, all_events_and_fight_links
             else:
-                new_events_and_fight_links = self.get_fight_links(self.new_event_links)
+                new_events_and_fight_links = get_fight_links(self.new_event_links)
 
-        all_events_and_fight_links = self.get_fight_links(self.all_event_links)
+        all_events_and_fight_links = get_fight_links(self.all_event_links)
         pickle_out = open(EVENT_AND_FIGHT_LINKS_PICKLE_PATH.as_posix(), "wb")
         pickle.dump(all_events_and_fight_links, pickle_out)
         pickle_out.close()
