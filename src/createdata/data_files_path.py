c2cd746299dd855eca4b0ceb3e58851761caf81a
WarrierRajeev <warrier.rajeev.95@gmail.com>
2020-09-28 10:02:02 +0530

    Update base path and add a new file path

--- src/createdata/data_files_path.py
+++ src/createdata/data_files_path.py
@@ -1,9 +1,10 @@
 import os
 from pathlib import Path
 
-BASE_PATH = Path(os.getcwd()).parents[0] / "data"
+BASE_PATH = Path(os.getcwd()) / "data"
 EVENT_AND_FIGHT_LINKS_PICKLE_PATH = BASE_PATH / "event_and_fight_links.pickle"
 PAST_EVENT_LINKS_PICKLE_PATH = BASE_PATH / "past_event_links.pickle"
+PAST_FIGHTER_LINKS_PICKLE_PATH = BASE_PATH / "past_fighter_links.pickle"
 NEW_EVENT_AND_FIGHTS = BASE_PATH / "new_fight_data.csv"
 TOTAL_EVENT_AND_FIGHTS = BASE_PATH / "total_fight_data.csv"
 PREPROCESSED_DATA = BASE_PATH / "preprocessed_data.csv"
