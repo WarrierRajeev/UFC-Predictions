8194c83d4ccd0bc4d188da95a43bf0dd3face188
WarrierRajeev <warrier.rajeev.95@gmail.com>
2020-10-01 12:13:25 +0530

    Update data files paths

--- src/createdata/data_files_path.py
+++ src/createdata/data_files_path.py
@@ -2,9 +2,10 @@ import os
 from pathlib import Path
 
 BASE_PATH = Path(os.getcwd()) / "data"
-EVENT_AND_FIGHT_LINKS_PICKLE_PATH = BASE_PATH / "event_and_fight_links.pickle"
-PAST_EVENT_LINKS_PICKLE_PATH = BASE_PATH / "past_event_links.pickle"
-PAST_FIGHTER_LINKS_PICKLE_PATH = BASE_PATH / "past_fighter_links.pickle"
+EVENT_AND_FIGHT_LINKS_PICKLE = BASE_PATH / "event_and_fight_links.pickle"
+PAST_EVENT_LINKS_PICKLE = BASE_PATH / "past_event_links.pickle"
+PAST_FIGHTER_LINKS_PICKLE = BASE_PATH / "past_fighter_links.pickle"
+SCRAPED_FIGHTER_DATA_DICT_PICKLE = BASE_PATH / "scraped_fighter_data_dict.pickle"
 NEW_EVENT_AND_FIGHTS = BASE_PATH / "new_fight_data.csv"
 TOTAL_EVENT_AND_FIGHTS = BASE_PATH / "total_fight_data.csv"
 PREPROCESSED_DATA = BASE_PATH / "preprocessed_data.csv"
